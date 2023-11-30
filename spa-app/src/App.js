75fd264ee3ee2a24f24b5fe524c6bade78b89c99
mtk.sugi <mtk.sugi@gmail.com>
2023-12-01 00:36:35 +0900

    upd: robotoフォントをCDNに

--- spa-app/src/App.js
+++ spa-app/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppRouter from './routers/AppRouters';
 import 'normalize.css/normalize.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
 import './styles/styles.scss'
 
 const root = createRoot(document.getElementById('app'));
